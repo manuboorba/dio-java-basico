34bdd278416af37b6c5b8f1d315325a50231dcad
Manuela Cavalcanti <manuelacavalcanti3@gmail.com>
2024-05-14 12:01:17 -0300

    Aprendendo anatomia das classes

--- java-anatomia-classes/src/main/java/MinhaClasse.java
+++ java-anatomia-classes/src/main/java/MinhaClasse.java
@@ -1,5 +1,28 @@
 public class MinhaClasse {
     public static void main(String[] args) {
         System.out.println("Olá mundo");
+
+        int ano = 2021;
+
+        ano = 2022;
+
+        final String BR = "Brasil";
+
+    //    BR = "Brazil";
+
+        double PI = 3.14;
+
+        int ESTADOS_BRASILEIROS = 27;
+
+        int ANO_2000 = 2000;
+
+        String meuNome = "Manuela";
+
+        int anoFabricacao = 2022;
+
+        boolean verdadeira = false;
+
+        anoFabricacao = 2018;
+
     }
 }
