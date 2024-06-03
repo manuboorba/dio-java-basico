b2986a5f30a77bea2138f7bd2ec667048c4fd769
Manuela <manuelacavalcanti3@gmail.com>
2024-06-03 19:30:27 -0300

    atualizei as mensagens mandadas ao usuário

--- conta/banco/ContaTerminal.java
+++ conta/banco/ContaTerminal.java
@@ -5,16 +5,16 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Por favor, digite o número da agência!");
+        System.out.println("Por favor, digite o número da agência");
         String agencia = scanner.next();
 
-        System.out.println("Por favor, digite o número da sua conta!");
+        System.out.println("Por favor, digite o número da sua conta");
         int numero = scanner.nextInt();
 
         System.out.println("Por favor, digite seu nome");
         String nome = scanner.next();
 
-        System.out.println("Por favor, digite seu saldo!");
+        System.out.println("Por favor, digite seu saldo");
         double saldo = scanner.nextDouble();
 
         System.out.println("Olá, " + nome + ". Obrigado por criar uma conta em nosso banco! Sua agência é " + agencia + ",conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
