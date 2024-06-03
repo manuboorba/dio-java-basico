4aef4c2076a733f44659be8c82aaf4ac7cb2bef5
Manuela <manuelacavalcanti3@gmail.com>
2024-06-03 15:50:58 -0300

    to do

--- conta-banco/ContaTerminal.java
+++ conta-banco/ContaTerminal.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class ContaTerminal {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        /*TO DO: Conhecer e importar a classe scanner
+         * Exibir as mensagens para o usuário
+         * Obter pela scanner os valores digitados no terminal
+         * Exibir a mensagem conta criada
+         */
     }
 }
